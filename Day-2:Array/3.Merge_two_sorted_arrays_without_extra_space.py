a05f5c1102264ea37212c8a4808eb8cfb31d9758
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-08-01 16:21:35 +0530

    Create 3.Merge_two_sorted_arrays_without_extra_space.py

---
+++ Day-2:Array/3.Merge_two_sorted_arrays_without_extra_space.py
@@ -0,0 +1,18 @@
+#  Problem :-https://leetcode.com/problems/merge-sorted-array/description/
+
+# Solution :- https://www.youtube.com/watch?v=n7uwj04E0I4&ab_channel=takeUforward
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
