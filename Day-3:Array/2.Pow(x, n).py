7e832b136a602c7cc0550f187c1a093510642105
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-08-02 17:12:57 +0530

    Create 2.Pow(x, n).py

---
+++ Day-3:Array/2.Pow(x, n).py
@@ -0,0 +1,8 @@
+# Problem :- https://leetcode.com/problems/powx-n/description/
+
+# Solution :- https://www.youtube.com/watch?v=l0YC3876qxg&ab_channel=takeUforward
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+        
