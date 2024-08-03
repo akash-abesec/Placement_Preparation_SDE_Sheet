f3b5b3b918122bdb2ada447aebe56868d2fcf835
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-08-03 15:25:31 +0530

    Update 1.2_SUM.py

--- Day-4:Array/1.2_SUM.py
+++ Day-4:Array/1.2_SUM.py
@@ -1,3 +1,7 @@
+# Problem :-https://leetcode.com/problems/two-sum/description/
+
+# Solution :- https://www.youtube.com/watch?v=UXDSeD9mN-k&ab_channel=takeUforward
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(0,len(nums)):
