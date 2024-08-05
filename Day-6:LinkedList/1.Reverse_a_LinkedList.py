8b676369bcde5933e3643a91f8304e4553842555
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-08-05 23:35:04 +0530

    Update 1.Reverse_a_LinkedList.py

--- Day-6:LinkedList/1.Reverse_a_LinkedList.py
+++ Day-6:LinkedList/1.Reverse_a_LinkedList.py
@@ -1,1 +1,17 @@
+# Problem :- https://leetcode.com/problems/reverse-linked-list/
 
+# Solution :- https://www.youtube.com/watch?v=iRtLEoL-r-g&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=28&ab_channel=takeUforward
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None 
+        curr = head
+        if head == None or head.next == None:
+            return head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
