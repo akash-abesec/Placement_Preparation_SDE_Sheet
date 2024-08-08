b78002f21ee9f0249276f92fb60a3a0c74c213d6
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-08-08 23:24:16 +0530

    Create 2.Detect_a_cycle_in_Linked List.py

---
+++ Day-6:LinkedList/2.Detect_a_cycle_in_Linked List.py
@@ -0,0 +1,19 @@
+# Problem :- https://leetcode.com/problems/linked-list-cycle/description/
+
+# Solution :- https://www.youtube.com/watch?v=354J83hX7RI&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=35
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast : 
+                return True
+        return False
