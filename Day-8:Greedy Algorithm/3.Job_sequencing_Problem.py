edec7faef7cbab6050a0a3edee63267269e35d83
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-08-12 00:03:51 +0530

    Create 3.Job_sequencing_Problem.py

---
+++ Day-8:Greedy Algorithm/3.Job_sequencing_Problem.py
@@ -0,0 +1,44 @@
+# Problem :- https://www.geeksforgeeks.org/problems/job-sequencing-problem-1587115620/1
+
+# Solution :- https://www.youtube.com/watch?v=QbwltemZbRg
+
+
+#User function Template for python3
+'''
+class Job:
+    
+    # Job class which stores profit and deadline.
+    
+    def __init__(self,profit=0,deadline=0):
+        self.profit = profit
+        self.deadline = deadline
+        self.id = 0
+'''        
+
+class Solution:
+    
+    #Function to find the maximum profit and the number of jobs done.
+    def JobScheduling(self,Jobs,n):
+        
+        # code here
+        Jobs.sort(key=lambda x: x.profit, reverse=True)
+        maxi = Jobs[0].deadline
+        for i in range(1, len(Jobs)):
+            maxi = max(maxi, Jobs[i].deadline)
+
+
+        slot = [-1] * (maxi + 1)
+        countJobs = 0
+        jobProfit = 0
+
+
+        for i in range(len(Jobs)):
+            for j in range(Jobs[i].deadline, 0, -1):
+                if slot[j] == -1:
+                    slot[j] = i
+                    countJobs += 1
+                    jobProfit += Jobs[i].profit
+                    break
+
+
+        return countJobs, jobProfit
