e8d21db430d59520a9d48a8305964c203297fa7c
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-08-12 20:12:55 +0530

    Create 6.Assign Cookies.py

---
+++ Day-8:Greedy Algorithm/6.Assign Cookies.py
@@ -0,0 +1,16 @@
+# Problem:- https://leetcode.com/problems/assign-cookies/description/
+
+# Solution :- https://www.youtube.com/watch?v=DIX2p7vb9co
+
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        content_children = 0
+        cookie_index = 0
+        while cookie_index < len(s) and content_children < len(g):
+            if s[cookie_index] >= g[content_children]:
+                content_children += 1
+            cookie_index += 1
+        return content_children
