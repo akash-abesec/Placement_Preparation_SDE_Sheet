fc4437a3863aeb21972584e029919de0c2ad9afa
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-08-13 20:43:58 +0530

    Update 1.Subset Sums.py

--- Day-9:Recursion/1.Subset Sums.py
+++ Day-9:Recursion/1.Subset Sums.py
@@ -3,3 +3,17 @@
 # Solution :- https://www.youtube.com/watch?v=rYkfBRtMJr8&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=53
 
 
+#User function Template for python3
+class Solution:
+	def subsetSums(self, arr, n):
+		# code here
+        ans = []
+        def subsetsumshepler(ind, sum1):
+            if ind == n:
+                ans.append(sum1)
+                return 
+            subsetsumshepler(ind+1, sum1+arr[ind])
+            subsetsumshepler(ind+1, sum1)
+        subsetsumshepler(0,0)
+        ans.sort()
+        return ans
